3521272ba56028903df57d4ce23f9711686b02f1
wowgarcon <61302719+wowgarcon@users.noreply.github.com>
2022-07-29 16:02:50 +0900

    코드 테스트 시작

---
+++ 로봇청소기.js
@@ -0,0 +1,19 @@
+
+
+
+function cleaning(N, M){
+    
+    let height = N;
+    let width = M;
+
+    let room = [];
+    for(i=0; i<width; i++){
+        for(j=0; j<height; j++){
+            room[i] = [];
+        }       
+    }
+    console.log(room.length);
+}
+
+console.log('aaa');
+cleaning(10, 10);
